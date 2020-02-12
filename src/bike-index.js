a1081d5c00b3574439bcd38f3035867213b7044c
Todd Walraven and Zakkrey Short <zakkrey@>
2020-02-12 14:27:55 -0800

    return bikes in city and color selected

--- src/bike-index.js
+++ src/bike-index.js
@@ -8,5 +8,18 @@ export class Bikes {
     } catch(error){
       console.error("There was an error handling your request: " + error.message);
     }
-  }
+  };
+
+  async getBikesByColor(color){
+    try{
+    let answer = await fetch(`https://bikeindex.org:443/api/v3/search?page=1&per_page=10&query=${color}&location=${city}&distance=10&stolenness=proximity`);
+    let jsonifiedAnswer = await answer.json();
+    return jsonifiedAnswer;
+    } catch(error){
+      console.error("There was an error handling your request: " + error.message);
+    }
+  };
+
+
 }
+
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import './bike-index';
-import { Bikes } from './bike-index.js';
+import { Bikes } from './bike-index.js'
 
 
 
@@ -12,16 +12,24 @@ $(document).ready(function(){
   $("form").submit(function(event) {
     event.preventDefault();
     const city = $("input#city").val();
+    const color = $("#colorSelect").val();
 
     (async () => {
       let bikes = new Bikes();
       const response = await bikes.getBikesByCity(city);
-      getElements(response);
+      const answer = await bikes.getBikesByColor(color);
+      getElements(response, answer);
     })();
 
-    function getElements(response) {
+    function getElements(response, answer) {
+      let bikeColorArray = [];
+      for(let i=0; i<10; i++){
+        bikeColorArray.push(answer.bikes[0].frame_colors)
+      }
+      console.log(answer)
       $('#outputCity').text(`The city you are searching is ${city}.`);
-      $('#outputResult').text(`The amount of bikes registered is: ${response.proximity}`);
+      $('#outputResult').text(`The amount of bikes registered here is: ${response.proximity}`);
+      $("#colorOutput").text(`The amount of bikes that are ${color} and ${bikeColorArray.length}`)
     };
     });
     })
